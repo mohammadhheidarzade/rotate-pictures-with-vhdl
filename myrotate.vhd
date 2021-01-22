a4ee7b8179fb285e2ab6077c9d5c021a55eb6b2a
Mohammad Hosein Heydarzadeh <dr.mhheidarzade@gmail.com>
2021-01-22 18:40:12 +0330

    some refactor

--- myrotate.vhd
+++ myrotate.vhd
@@ -2,20 +2,20 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
-PACKAGE types is    -- untested...
-    type header_type  is array (0 to 53) of STD_LOGIC_VECTOR(7 DOWNTO 0);
+PACKAGE types IS    -- untested...
+    TYPE header_type  IS ARRAY (0 TO 53) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
     TYPE image_type IS ARRAY (0 TO 3000010) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
 END types;
 PACKAGE BODY types IS
 END types;
 
-USE work.types.all;
+USE work.types.ALL;
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 ENTITY rotate IS
   PORT (
-    start           :   IN     STD_LOGIC ;
+    start           :   IN     STD_LOGIC;
     degree          :   IN     INTEGER RANGE 3 DOWNTO 0;
     pic_width_in    :   IN     INTEGER RANGE 2048 DOWNTO 1; 
     pic_height_in   :   IN     INTEGER RANGE 2048 DOWNTO 1;
@@ -28,72 +28,62 @@ ENTITY rotate IS
 END rotate;
 
 ARCHITECTURE behavioral OF rotate IS
-    
 BEGIN
     PROCESS
-        VARIABLE sinf               :   INTEGER;
-        VARIABLE cosf               :   INTEGER;
-        VARIABLE x0                 :   INTEGER;
-        VARIABLE y0                 :   INTEGER;
-
-        VARIABLE x0_2                 :   INTEGER;
-        VARIABLE y0_2                 :   INTEGER;
-
-        VARIABLE xx                 :   INTEGER;
-        VARIABLE yy                 :   INTEGER;
-        VARIABLE a                  :   INTEGER;
-        VARIABLE b                  :   INTEGER;
-
-        VARIABLE a_2                  :   INTEGER;
-        VARIABLE b_2                  :   INTEGER;
-
-        VARIABLE x                  :   INTEGER;
-        VARIABLE y                  :   INTEGER;
-        VARIABLE image_out_TEMP     :   image_type;
+        VARIABLE sinf                    :   INTEGER;
+        VARIABLE cosf                    :   INTEGER;
+        VARIABLE x0_2                    :   INTEGER;
+        VARIABLE y0_2                    :   INTEGER;
+        VARIABLE xx                      :   INTEGER;
+        VARIABLE yy                      :   INTEGER;
+        VARIABLE a                       :   INTEGER;
+        VARIABLE b                       :   INTEGER;
+        VARIABLE x                       :   INTEGER;
+        VARIABLE y                       :   INTEGER;
+        VARIABLE image_out_TEMP          :   image_type;
         VARIABLE pic_width_out_temp      :   Integer RANGE 2048 DOWNTO 1;
         VARIABLE pic_height_out_temp     :   Integer RANGE 2048 DOWNTO 1;
-
     BEGIN
-        WAIT UNTIL (start'EVENT and start='1');
-        
-        -- x0 := (pic_width_in - 1) / 2;
-        -- y0 := (pic_height_in - 1) / 2;
+        WAIT UNTIL (start'EVENT AND start='1');
+
         x0_2 := (pic_width_in - 1) ;
         y0_2 := (pic_height_in - 1) ;
-
-        report "width: " & integer'image(pic_width_in);
-        report "height: " & integer'image(pic_height_in);
 
         IF degree = 0 THEN
             sinf := 0;
             cosf := 1;
+
             pic_width_out_temp  :=  pic_width_in  ;
             pic_height_out_temp :=  pic_height_in  ;
         ELSIF degree = 1 THEN
             sinf := 1;
             cosf := 0;
+
             pic_width_out_temp  :=  pic_height_in  ;
             pic_height_out_temp :=  pic_width_in  ;
         ELSIF degree = 2 THEN
             sinf := 0;
             cosf := -1;
+
             pic_width_out_temp  :=  pic_width_in  ;
             pic_height_out_temp :=  pic_height_in  ;
         ELSIF degree = 3 THEN
             sinf := -1;
             cosf := 0;
+
             pic_width_out_temp  :=  pic_height_in  ;
             pic_height_out_temp :=  pic_width_in  ;
         END IF;
-        
         
         FOR x IN 0 TO pic_width_in - 1 LOOP
             FOR y IN 0 TO pic_height_in - 1 LOOP
                 a :=   ( 2*x - x0_2) ;
                 b :=   ( 2*y - y0_2) ;
+
                 xx :=  ((a * cosf) - (b * sinf) + x0_2 ) / 2;
                 yy :=  ((a * sinf) + (b * cosf) + y0_2 ) / 2;
-                IF xx >= 0 and xx < pic_width_in and yy >= 0 and yy < pic_height_in THEN
+
+                IF xx >= 0 AND xx < pic_width_in AND yy >= 0 AND yy < pic_height_in THEN
                     image_out_TEMP((y * pic_width_in + x) * 3 + 0) := image_in((yy * pic_width_in + xx) * 3 + 0);
                     image_out_TEMP((y * pic_width_in + x) * 3 + 1) := image_in((yy * pic_width_in + xx) * 3 + 1);
                     image_out_TEMP((y * pic_width_in + x) * 3 + 2) := image_in((yy * pic_width_in + xx) * 3 + 2);
@@ -105,8 +95,5 @@ BEGIN
         image_out       <= image_out_TEMP;
         pic_width_out   <= pic_width_out_temp;
         pic_height_out  <= pic_height_out_temp;
-        report "finish myrotate";
     END PROCESS ; -- process
-    
-    
-END behavioral ; -- arch
+END behavioral ;  -- arch
--- myrotate_tb.vhd
+++ myrotate_tb.vhd
@@ -1,41 +1,37 @@
-USE work.types.all;
-use std.textio.all;
-use std.env.finish;
-USE work.types.all;
-
 LIBRARY ieee;
+USE work.types.ALL;
+USE std.textio.ALL;
+USE work.types.ALL;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY rotate_tb IS
 END rotate_tb;
+
 ARCHITECTURE behavioral OF rotate_tb IS
     COMPONENT rotate IS
     PORT (
-        start           :   IN     STD_LOGIC;
-        degree          :   IN     INTEGER RANGE 3 DOWNTO 0;
-        
-        pic_width_in    :   IN     INTEGER RANGE 2048 DOWNTO 1; 
-        pic_height_in   :   IN     INTEGER RANGE 2048 DOWNTO 1;
-        image_in        :   IN     image_type;
+        start                   :   IN     STD_LOGIC;
+        degree                  :   IN     INTEGER RANGE 3 DOWNTO 0;
+        pic_width_in            :   IN     INTEGER RANGE 2048 DOWNTO 1; 
+        pic_height_in           :   IN     INTEGER RANGE 2048 DOWNTO 1;
+        image_in                :   IN     image_type;
 
-        pic_width_out   :   OUT    INTEGER RANGE 2048 DOWNTO 1; 
-        pic_height_out  :   OUT    INTEGER RANGE 2048 DOWNTO 1;
-        image_out       :   OUT    image_type
+        pic_width_out           :   OUT    INTEGER RANGE 2048 DOWNTO 1; 
+        pic_height_out          :   OUT    INTEGER RANGE 2048 DOWNTO 1;
+        image_out               :   OUT    image_type
     );
-    END COMPONENT;
-    
+    END COMPONENT;  
 
-    SIGNAL degree_tb          :        INTEGER RANGE 3 DOWNTO 0;
-    SIGNAL pic_width_in_tb    :        INTEGER RANGE 2048 DOWNTO 1; 
-    SIGNAL pic_height_in_tb   :        INTEGER RANGE 2048 DOWNTO 1;
-    SIGNAL image_in_tb        :        image_type;
-    SIGNAL pic_width_out_tb   :        INTEGER RANGE 2048 DOWNTO 1; 
-    SIGNAL pic_height_out_tb  :        INTEGER RANGE 2048 DOWNTO 1;
-    SIGNAL image_out_tb       :        image_type;
-    SIGNAL start_tb           :     STD_LOGIC := '0';
-
-    SIGNAL key                :     STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+    SIGNAL degree_tb            :        INTEGER RANGE 3 DOWNTO 0;
+    SIGNAL pic_width_in_tb      :        INTEGER RANGE 2048 DOWNTO 1; 
+    SIGNAL pic_height_in_tb     :        INTEGER RANGE 2048 DOWNTO 1;
+    SIGNAL image_in_tb          :        image_type;
+    SIGNAL pic_width_out_tb     :        INTEGER RANGE 2048 DOWNTO 1; 
+    SIGNAL pic_height_out_tb    :        INTEGER RANGE 2048 DOWNTO 1;
+    SIGNAL image_out_tb         :        image_type;
+    SIGNAL start_tb             :        STD_LOGIC := '0';
+    SIGNAL key                  :        STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
     SIGNAL header_sig : header_type;
 BEGIN
     CUT : rotate 
@@ -49,80 +45,95 @@ BEGIN
         pic_height_out_tb,
         image_out_tb     
     );
+
     key <= "00" ,"01" AFTER 10 ns ,"10" AFTER 20 ns,"11" AFTER 30 ns;
+
     read_p : PROCESS
         TYPE char_file IS FILE OF CHARACTER;
-        FILE bmp_file : char_file OPEN read_mode IS "test3.bmp";
+        FILE bmp_file : char_file OPEN read_mode IS "test2.bmp";
         VARIABLE header : header_type;
         VARIABLE image_in_width  : INTEGER;
         VARIABLE image_in_height : INTEGER;
         VARIABLE char : CHARACTER;
         VARIABLE image: image_type;
     BEGIN
-        WAIT UNTIL ( key'EVENT and key="01");
-        report "start reading";
+        WAIT UNTIL ( key'EVENT AND key="01");
+        
         FOR i IN 0 TO 53 LOOP
             read(bmp_file, char);
             header(i) :=
                     std_logic_vector(to_unsigned(character'pos(char), 8));
         END LOOP;
+
         header_sig <= header;
+
         image_in_width :=to_integer(unsigned(header(18))) +
         to_integer(unsigned(header(19))) * 2**8 +
         to_integer(unsigned(header(20))) * 2**16 +
         to_integer(unsigned(header(21))) * 2**24;
+
         image_in_height := to_integer(unsigned(header(22))) +
         to_integer(unsigned(header(23))) * 2**8 +
         to_integer(unsigned(header(24))) * 2**16 +
         to_integer(unsigned(header(25))) * 2**24;
+
         FOR i IN 0 TO (image_in_height * image_in_width * 3 - 1) LOOP
             read(bmp_file, char);
-            image(i) :=
-                    std_logic_vector(to_unsigned(character'pos(char), 8));
+            image(i) := std_logic_vector(to_unsigned(character'pos(char), 8));
         END LOOP;
-        file_close(bmp_file);
-        degree_tb <= 3;
+
+        degree_tb <= 1; -- degree
         image_in_tb <= image;
         image_in_tb <= image;
         pic_width_in_tb <= image_in_width;
         pic_height_in_tb <= image_in_height;
         start_tb <= '1';
+
+        file_close(bmp_file);
     END PROCESS read_p;    
+
     write_P : PROCESS
         TYPE char_file IS FILE OF CHARACTER;
-        FILE out_file : char_file OPEN write_mode IS "out1.bmp";
-        VARIABLE image_out_width : INTEGER;
-        VARIABLE image_out_height : INTEGER;
-        VARIABLE char : CHARACTER;
-        VARIABLE header : header_type;
-        VARIABLE temp_integer : INTEGER;
-    begin
-        WAIT UNTIL ( key'EVENT and key="10");
-        report "start writing";
+        FILE out_file               :       char_file OPEN write_mode IS "out1.bmp";
+        VARIABLE image_out_width    :       INTEGER;
+        VARIABLE image_out_height   :       INTEGER;
+        VARIABLE char               :       CHARACTER;
+        VARIABLE header             :       header_type;
+        VARIABLE temp_integer       :       INTEGER;
+    BEGIN
+        WAIT UNTIL ( key'EVENT AND key="10");
+        
         header := header_sig;
+
         image_out_width  := pic_width_out_tb;
         image_out_height := pic_height_out_tb;
+
         FOR i IN 0 TO 53 LOOP
-            IF i >= 18 and i<=21 THEN
+            IF i >= 18 AND i<=21 THEN
                 temp_integer := image_out_width MOD 2**8;
                 char := character'val(temp_integer);
                 image_out_width := image_out_width - temp_integer;
                 image_out_width := image_out_width / 2**8;
+
                 write(out_file, char);  
-            ELSIF i >= 22 and i<=25 THEN
+            ELSIF i >= 22 AND i<=25 THEN
                 temp_integer := image_out_height MOD 2**8;
                 char := character'val(temp_integer);
                 image_out_height := image_out_height - temp_integer;
                 image_out_height := image_out_height / 2**8; 
+
                 write(out_file, char);  
             ELSE
                 write(out_file, character'val(to_integer(unsigned(header(i)))));
             END IF;
         END LOOP;
+
         FOR i IN 0 TO (pic_height_out_tb * pic_width_out_tb * 3 - 1) LOOP
             write(out_file, character'val(to_integer(unsigned(image_out_tb(i)))));
-        end loop;
+        END LOOP;
+
         file_close(out_file);
-        report "Simulation done. Check ""out.bmp"" image.";
+        
+        REPORT "END of SIMULATION!";
     END PROCESS write_P;
 END behavioral ;
